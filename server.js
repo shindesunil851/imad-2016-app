c5fd177ac416ee50fd87008e227a6a47531be901
sunil shinde <shindesunil851@gmail.com>
2016-09-22 13:27:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/article2', function (req, res) {
+  res.send('Article 2 will be served here');
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
