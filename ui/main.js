3bb69988c3a825a21fc91d9c349b24406ef981fa
sunil shinde <shindesunil851@gmail.com>
2016-09-23 15:23:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 //remove content
 var element = document.getElementById('ss');
 element.innerHTML = 'elements removed';
-//
+//dfsdag
 var madi = document.getElementById('madi');
 madi.onclick = function () {
     madi.style.marginleft = "10px";
