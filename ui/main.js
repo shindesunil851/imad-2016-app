764216cc7f4741e6a245d8808d2c722204286eeb
sunil shinde <shindesunil851@gmail.com>
2016-09-23 15:03:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,8 @@
 console.log('Loaded!');
-
+//remove content
+var element = getElementById('ss');
+element.innerHTML = 'HI Everyone';
+//
 var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginleft = "100px";
